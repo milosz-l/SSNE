8e3a5652c067626bf2f8da2a90ec18c345321d28
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-01-10 00:44:41 +0100

    created general notebook for all models and added file with all imports

---
+++ lab12_miniprojekt5/VAE.py
@@ -0,0 +1,68 @@
+from all_imports import *
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super(Encoder, self).__init__()
+
+        self.fc_1 = nn.Linear(input_dim, hidden_dim)
+        self.fc_2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_mean  = nn.Linear(hidden_dim, latent_dim)
+        self.fc_var   = nn.Linear(hidden_dim, latent_dim)
+        
+        self.LeakyReLU = nn.LeakyReLU(0.2)
+        
+        self.training = True
+        
+    def forward(self, x):
+        x = torch.flatten(x, 1) # spłaszczamy obrazek do długiego wektora (to głupie trochę, robione dla przykładu na labki)
+        x       = self.LeakyReLU(self.fc_1(x))
+        x       = self.LeakyReLU(self.fc_2(x))
+        mean     = self.fc_mean(x)
+        log_var  = self.fc_var(x)                      # encoder produces mean and log of variance 
+                                                       #             (i.e., parateters of simple tractable normal distribution "q"
+        
+        return mean, log_var
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, output_dim):
+        super(Decoder, self).__init__()
+        self.fc_1 = nn.Linear(latent_dim, hidden_dim)
+        self.fc_2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_3 = nn.Linear(hidden_dim, output_dim)
+        
+        self.LeakyReLU = nn.LeakyReLU(0.2)
+        
+    def forward(self, x):
+        h     = self.LeakyReLU(self.fc_1(x))
+        h     = self.LeakyReLU(self.fc_2(h))
+        
+        x_hat = torch.sigmoid(self.fc_3(h))
+        x_hat = x_hat.view([-1, 3, 32, 32])
+        return x_hat
+
+
+class VAE(nn.Module):
+    def __init__(self, x_dim, hidden_dim, latent_dim):
+        super(VAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.encoder = Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
+        self.decoder = Decoder(latent_dim=latent_dim, hidden_dim = hidden_dim, output_dim = x_dim)
+
+        
+    def reparameterization(self, mean, var):
+        eps = torch.randn_like(mean)
+        z = eps * var + mean
+        # z = np.random.normal(mean, var)
+        # z = mean # Change to proper sampling
+        return z
+        
+                
+    def forward(self, x):
+        mean, log_var = self.encoder(x)
+#         print("mean log_var", mean.shape, log_var.shape)
+        z = self.reparameterization(mean, torch.exp(0.5 * log_var)) # takes exponential function (log var -> var)
+#         print("z", z.shape)
+        x_hat = self.decoder(z)
+        return x_hat, mean, log_var
---
+++ lab12_miniprojekt5/all_imports.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+import torchvision
+import torchvision.transforms as transforms
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+## PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+
+import torch.optim as optim
+
+from torch.utils.data import DataLoader, random_split
